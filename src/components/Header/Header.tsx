a42961121b18f5c4de122abb101a7a47fb83c58c
김진근 <freedom7113@gmail.com>
2021-11-25 00:22:20 +0900

    ⚡️ :: (#4) add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,32 @@
+import { useLayoutEffect, useState } from "react";
 import * as S from "./styles";
+import back from "../../assets/icons/back.svg";
+import logout from "../../assets/icons/Logout.svg";
+import { useNavigate } from "react-router";
 
 const Header = (): JSX.Element => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState<string>("");
+
+  const onBack = () => {
+    navigate(-1);
+  };
+
+  useLayoutEffect(() => {
+    setTitle(document.title);
+  }, [document.title]);
+
+  useLayoutEffect(() => {
+    document.title = "롯데리아 신성점";
+  }, []);
+
   return (
     <>
-      <S.Container></S.Container>
+      <S.Container>
+        <S.Back src={back} onClick={onBack} />
+        <S.Title>{title}</S.Title>
+        <S.Logout src={logout} />
+      </S.Container>
     </>
   );
 };
--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,13 +1,13 @@
 import React, { FC, Suspense } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { MainContainer } from "../container";
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>로딩중...</div>}>
-      <Switch>
-        <Route exact path="/" component={MainContainer} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<MainContainer />} />
+      </Routes>
     </Suspense>
   );
 };
