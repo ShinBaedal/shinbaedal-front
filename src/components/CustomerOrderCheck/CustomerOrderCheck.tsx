42a70e2297317c2a55edf265ceb8e64e4f35664c
J1min <jmhong101506@gmail.com>
2021-11-25 01:11:20 +0900

    ⚡️ :: (#3) 완성
    고객 주문 페이지 완성(헤더x)

--- src/components/CustomerOrderCheck/CustomerOrderCheck.tsx
+++ src/components/CustomerOrderCheck/CustomerOrderCheck.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import * as S from "./styles";
+import SelectedMenu from "./SelectedMenu";
+import OrderBtn from "./OrderBtn";
 // import font from "../../style/font"
 
 export default function CustomerOrderCheck() {
   return (
     <>
       <S.Selected>선택한 메뉴</S.Selected>
-      <S.SelectedMenu>
-        
-          <img
-            src="https://ifh.cc/g/C6wP44.png"
-            alt=""
-            width="100"
-            height="100"
-            style={{ background: "black", borderRadius: "5px" }}
-          />
-        <p>2월17일 발행</p>
-        <hr />
-      </S.SelectedMenu>
+      <SelectedMenu></SelectedMenu>
+      <SelectedMenu></SelectedMenu>
+      <SelectedMenu></SelectedMenu>
+      <OrderBtn></OrderBtn>
     </>
   );
 }
