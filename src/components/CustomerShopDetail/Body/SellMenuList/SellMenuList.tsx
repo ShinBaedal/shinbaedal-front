ebb541f7db94ff79244aae02f01893cdfd6f853b
김진근 <freedom7113@gmail.com>
2021-11-25 03:30:22 +0900

    ⚡️ :: (#4) add sell menu list

--- src/components/CustomerShopDetail/Body/SellMenuList/SellMenuList.tsx
+++ src/components/CustomerShopDetail/Body/SellMenuList/SellMenuList.tsx
@@ -1,3 +1,15 @@
-const SellMenuList = () => {};
+import SellMenu from "../../SellMenu/SellMenu";
+import * as S from "./styles";
+const SellMenuList = () => {
+  return (
+    <S.Container>
+      <SellMenu />
+      <SellMenu />
+      <SellMenu />
+      <SellMenu />
+      <SellMenu />
+    </S.Container>
+  );
+};
 
 export default SellMenuList;
--- src/components/CustomerShopDetail/Body/SellMenuList/styles.ts
+++ src/components/CustomerShopDetail/Body/SellMenuList/styles.ts
@@ -1,1 +1,9 @@
-export {};
+import styled from "@emotion/styled";
+import { color } from "../../../../style/color";
+
+export const Container = styled.div`
+  width: 100%;
+  padding-top: 16px;
+  padding-bottom: 36px;
+  background-color: ${color.white};
+`;
