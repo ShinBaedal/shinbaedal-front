1a4c574a2cc9f41769ff443551f75e83a66318a5
김진근 <freedom7113@gmail.com>
2021-11-25 01:53:37 +0900

    ⚡️ :: (#4) change props

--- src/components/CustomerShopDetail/Body/Menu/Menu.tsx
+++ src/components/CustomerShopDetail/Body/Menu/Menu.tsx
@@ -1,7 +1,9 @@
+import State from "../../../../interface/State";
 import * as S from "./styles";
 
 interface PropsType {
   navs: string[];
+  navState: State<string>;
 }
 
 const Menu = ({ navs }: PropsType): JSX.Element => {
