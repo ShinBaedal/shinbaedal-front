ac7edd32307d6611b985a8810213da9901949245
김진근 <freedom7113@gmail.com>
2021-11-25 02:39:55 +0900

    ⚡️ :: (#4) add navs

--- src/components/CustomerShopDetail/CustomerShopDetail.tsx
+++ src/components/CustomerShopDetail/CustomerShopDetail.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Menu from "./Body/Menu/Menu";
 import Header from "./Header/Header";
+import * as S from "./styles";
 
 const CustomerShopDetail = () => {
-  return <Header />;
+  const navs = ["메뉴", "리뷰"];
+  const [nav, setNav] = useState<string>(navs[0]);
+
+  return (
+    <S.Container>
+      <Header />
+      <Menu navs={navs} navState={[nav, setNav]} />
+    </S.Container>
+  );
 };
 
 export default CustomerShopDetail;
