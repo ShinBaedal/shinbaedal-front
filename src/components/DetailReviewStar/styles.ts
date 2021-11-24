86112afd9e17ea921b7897913839dd2375a0b7a5
J1min <jmhong101506@gmail.com>
2021-11-25 03:23:11 +0900

    ⚡️ :: (#8) complete
    complete

--- src/components/DetailReviewStar/styles.ts
+++ src/components/DetailReviewStar/styles.ts
@@ -7,7 +7,7 @@ export const MainDiv = styled.div`
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 300px;
+  height: 360px;
   border-radius: 20px;
   background-color: ${color.grey};
 `;
@@ -16,18 +16,20 @@ export const ReviewGo = styled.p`
   padding-top: 20px;
   padding-left: 20px;
   font: ${font.headline2};
+`;
 
-
-`
-
-
-
-
-
-
-
-
-
+export const ReviewInput = styled.input`
+  width: 90%;
+  padding-bottom: 120px;
+  display: block;
+  margin: 0 auto;
+  border-radius: 20px;
+  border: 0;
+  padding-top: 10px;
+  padding-left: 10px;
+  font: ${font.description1};
+  background-color: ${color.lightGrey};
+`;
 
 export const Fill = styled.div`
   position: fixed;
@@ -35,4 +37,21 @@ export const Fill = styled.div`
   width: 100%;
   height: 30px;
   background-color: ${color.grey};
+`;
+
+export const WriteBtn = styled.button`
+  width: 80%;
+  height: 35px;
+  margin-top: 30px;
+  margin-left: 37.5px;
+  display: block;
+  background-color: ${color.green};
+  border-radius: 20px;
+  border: 0;
+  font: ${font.body1};
+  color: white;
+`;
+
+export const Black = styled.div`
+  background-color: ${color.black};
 `;
