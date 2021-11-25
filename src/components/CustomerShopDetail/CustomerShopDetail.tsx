dd8421424fc9538726a3f67ef61a37137c0f5db4
김진근 <freedom7113@gmail.com>
2021-11-25 12:53:02 +0900

    ⚡️ :: (#12) change component by state

--- src/components/CustomerShopDetail/CustomerShopDetail.tsx
+++ src/components/CustomerShopDetail/CustomerShopDetail.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import CustomerDetailReview from "../CustomerDetailReview/CustomerDetailReview";
 import Menu from "./Body/Menu/Menu";
 import SellMenuList from "./Body/SellMenuList/SellMenuList";
 import Header from "./Header/Header";
@@ -8,12 +9,22 @@ const CustomerShopDetail = () => {
   const navs = ["메뉴", "리뷰"];
   const [nav, setNav] = useState<string>(navs[0]);
 
+  const renderBody = () => {
+    const bodyMap = new Map<string, () => JSX.Element>()
+      .set(navs[0], SellMenuList)
+      .set(navs[1], CustomerDetailReview);
+
+    const render = bodyMap.get(nav)!;
+
+    return React.createElement(render);
+  };
+
   return (
     <S.Container>
       <Header />
       <div>
         <Menu navs={navs} navState={[nav, setNav]} />
-        <SellMenuList />
+        {renderBody()}
       </div>
     </S.Container>
   );
