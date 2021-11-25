eef50ad87565246ac4bc615f4d585c01e3765473
김진근 <freedom7113@gmail.com>
2021-11-26 04:52:25 +0900

    ⚡️ :: chanve save

--- src/constance/uri/index.ts
+++ src/constance/uri/index.ts
@@ -1,11 +1,13 @@
-enum uri {
-  store = "/store",
-  menuList = "/menu/list",
-  review = "/review",
-  reviewList = "/review/list",
-  photo = "/photo",
-  order = "/order",
-  myOrder = "/order/my",
-}
+export const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+
+const uri = {
+  store: `${PROXY}/store`,
+  menuList: `${PROXY}/menu/list`,
+  review: `${PROXY}/review`,
+  reviewList: `${PROXY}/review/list`,
+  photo: `${PROXY}/photo`,
+  order: `${PROXY}/order`,
+  myOrder: `${PROXY}/order/my`,
+};
 
 export default uri;
